1be78c29155b643d9710d359b0c8c6251890beee
万俟淋曦 <1055311345@qq.com>
2020-06-03 19:48:15 +0800

    ACM算法分类

--- LeetCode/237.删除链表中的节点/237.删除链表中的节点.py
+++ LeetCode/237.删除链表中的节点/237.删除链表中的节点.py
@@ -6,8 +6,11 @@
 class Solution:
     def deleteNode(self, node):
         """
-        没有改变指针指向，只是用后一个节点覆盖了要删除的节点
+        用后一个节点覆盖了要删除的节点
+        在链表占用的存储空间上，实际删除的空间是要删除节点的下一个节点的空间
         """
         # 使用node.next找到后一个节点
+        # 用后一个节点的值覆盖要删除的节点值
         node.val = node.next.val
+        # 用后一个节点的指针覆盖要删除的节点指针
         node.next = node.next.next
