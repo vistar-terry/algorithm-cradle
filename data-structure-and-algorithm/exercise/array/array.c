4fe72a4e4bca7f5a150f69ca20362aa243b6e43b
Your Name <you@example.com>
2020-11-09 17:17:00 +0800

    init

--- data-structure-and-algorithm/exercise/array/array.c
+++ data-structure-and-algorithm/exercise/array/array.c
@@ -7,9 +7,8 @@ struct Table{
 };
 
 int main(int argc, char* argv[]){
-    
-    int arr[3] = {0};
     int i = 0;
+    int arr[3] = {0};
     for(; i<=3; i++){
         arr[i] = 0;
         printf("hello world\n");
