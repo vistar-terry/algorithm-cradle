dd837468ba3341f59a7fbdf438c5864392663f06
holsey <1055311345@qq.com>
2020-11-23 21:56:46 +0800

    olink

---
+++ data-structure-and-algorithm/6.数组(矩阵)/OLink/build/CMakeFiles/olink.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/olink.dir/olink.cpp.o"
+  "olink"
+  "olink.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/olink.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
